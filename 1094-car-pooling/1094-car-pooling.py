d0e53124d7578039d5cd39566653a15d530cc55e
Yeswanth Polu <73042500+YashPolu@users.noreply.github.com>
2022-01-05 21:24:47 -0500

    Time: 449 ms (5.19%), Space: 14.9 MB (51.55%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -3,22 +3,15 @@ class Solution:
         
         # print(max(max(trips)))
         
-        dp = [0] * max(max(trips, key=lambda x: x[2]))
+        dp = [0] * 1000
         
         trips.sort(key = lambda x: x[1])
-        
-        starts = [i[1] for i in trips]
-        
-        ends = [i[2] for i in trips]
-        
-        for i in range(len(trips)):
-            for j in range(starts[i],ends[i]):
-                dp[j] += trips[i][0]
+
+        for i in trips:
+            for j in range(i[1],i[2]):
+                dp[j] += i[0]
                 
-        if max(dp) > capacity:
-            return False
-        else:
-            return True
+        return max(dp) <= capacity
         
         
         
