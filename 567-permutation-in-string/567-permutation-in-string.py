bc18ace7b021015850ddcfa9084e11658158549f
Yeswanth Polu <73042500+YashPolu@users.noreply.github.com>
2022-01-07 10:00:10 -0500

    Time: 56 ms (96.66%), Space: 14.2 MB (86.91%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,38 +1,28 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        """
-        :type s1: str
-        :type s2: str
-        :rtype: bool
-        """
-        n = len(s1)
-        m = len(s2)
-        # If len of s1 is greater than s2, then permutation is not possible
-        if (n > m):
+        
+        n1 = len(s1)
+        n2 = len(s2)
+
+        if n1 > n2: 
             return False
-        
-        # Create 2 arrays of size 26 to store frequency of letters
-        x = [0] * 26
-        y = [0] * 26
-        
-        # Counting Frequency of s1 and storing it in 'array y'
-        for i in s1:
-            y[(ord(i) - ord('a'))] += 1
-        
-        # Counting Frequency of first s1.length th letters and storing them in 'array x'
-        for i in range(n):
-            x[(ord(s2[i]) - ord('a'))] += 1
-        
-        # Checking whether the first 's1 length'th letters are permutation of s1
-        if (x == y):
+
+        target = [0 for _ in range(26)]
+        window = [0 for _ in range(26)]
+
+        for i in range(n1):
+            target[ord(s1[i]) - 97] += 1
+            window[ord(s2[i]) - 97] += 1
+
+        if(target == window):
             return True
-        
-        # Else iterating a sliding window until we get permutation or array ends
-        start = 0
-        for i in range(n,m):
-            x[(ord(s2[start]) - ord('a'))] -= 1
-            x[(ord(s2[i]) - ord('a'))] += 1
-            start += 1
-            if (x == y):
+
+        for i in range(n1, n2):
+            window[ord(s2[i - n1]) - 97] -= 1
+            window[ord(s2[i]) - 97] += 1
+
+            if target == window:
                 return True
+
         return False
+                
