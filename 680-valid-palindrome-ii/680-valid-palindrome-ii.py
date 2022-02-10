1c20ce58b1e6ebcb3c3b02c805a4b461022ba04a
Yeswanth Polu <73042500+YashPolu@users.noreply.github.com>
2022-02-09 22:34:38 -0800

    Time: 106 ms (82.80%), Space: 14.5 MB (68.74%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        n = len(s)
+
+        if s == s[::-1]:
+            return True # return True if the string is already a palindrome
+        i = 0
+        j=n-1
+
+
+        while(i<=j):
+            if s[i] != s[j]: 
+                if s[i+1: j+1][::-1] == s[i+1: j+1] or s[i: j] == s[i: j][::-1]:
+                    return True
+                else:
+                    return False
+            else:
+                i += 1 # keep moving right
+                j -= 1 # keep moving left
