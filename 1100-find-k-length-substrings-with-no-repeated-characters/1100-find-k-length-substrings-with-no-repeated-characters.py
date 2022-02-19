33e575df6581979e37c3ee76c6777f003d60a76a
Yeswanth Polu <73042500+YashPolu@users.noreply.github.com>
2022-02-19 08:06:09 -0800

    Time: 92 ms (30.94%), Space: 14 MB (37.98%) - LeetHub

---
+++ 1100-find-k-length-substrings-with-no-repeated-characters/1100-find-k-length-substrings-with-no-repeated-characters.py
@@ -0,0 +1,32 @@
+class Solution:
+    def numKLenSubstrNoRepeats(self, s: str, k: int) -> int:
+        
+        l = len(s)
+        
+        if k > l:
+            return 0
+        
+        count = 0 
+        d = Counter(s[:k])
+        
+        if len(d.keys()) == k:
+            count+=1
+        
+        for i in range(k,l):
+            d[s[i]]+=1
+            
+            if i >= k:
+                if d[s[i-k]] == 1:
+                    del d[s[i-k]]
+                else:
+                    d[s[i-k]]-=1
+        
+            if len(d.keys()) == k:
+                count+=1
+            
+        
+        return count
+                
+        
+        
+        
